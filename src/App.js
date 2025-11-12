150f8e9ac076742bb04801d24ae587248f64ff93
Andres <joseandresmillan16@gmail.com>
2025-11-12 14:58:34 -0500

    Agregar rutas del blog en App.js
    - Agrega ruta /blog para listado de posts
    - Agrega ruta dinámica /blog/:slug para detalle
    - Integra componentes BlogPage y BlogDetail al router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Cases from "containers/pages/Cases";
 import Services from "containers/pages/Services";
 import About from "containers/pages/About";
 import Blog from "containers/pages/Blog";
+import BlogDetail from "components/blog/BlogDetail";
 import Contact from "containers/pages/Contact";
 import ApodPage from "containers/pages/ApodPage";
 import Muelles from "containers/pages/cases/Muelles";
@@ -25,6 +26,7 @@ function App() {
           <Route path="/servicios" element={<Services />} />
           <Route path="/nosotros" element={<About />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogDetail />} />
           <Route path="/contacto" element={<Contact />} />
           <Route path="/apod" element={<ApodPage />} /> 
           <Route path="*" element={<Error404 />} />
